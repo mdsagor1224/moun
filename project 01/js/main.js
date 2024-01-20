b646bc4f9ad2c93127fb8227a0b3748790b1ca7a
mdsagor <mdsagorahmed1224@gmail.com>
2024-01-20 10:52:09 +0600

    responsive

--- project 01/js/main.js
+++ project 01/js/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
 
+  // mainmenu
+  $('#mobile-menu-active').meanmenu({
+    meanScreenWidth: "991",
+    meanMenuContainer: '.mobile-menu'
+  });
+
   // slider-active
     $('.slider-active').slick({
         infinite: true,
@@ -24,7 +30,30 @@ $(document).ready(function(){
         arrows:false,
         dots:false,
         slidesToShow: 5,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 991,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 1,
+            }
+          },
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1
+            }
+          }
+        ]
       });
   
 
